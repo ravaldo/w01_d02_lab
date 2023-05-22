b11aa8b79406d28e1e870ab3cc3482e0c2151058
ravaldo <117010252+ravaldo@users.noreply.github.com>
2023-05-22 15:30:53 +0100

    first commit

---
+++ team_class/src/team.py
@@ -0,0 +1,17 @@
+
+class Team:
+	def __init__(self, name, players, coach):
+		self.name = name
+		self.players = players
+		self.coach = coach
+		self.points = 0
+
+	def add_player(self, player):
+		self.players.append(player)
+	
+	def has_player(self, player):
+		return True if player in self.players else False
+	
+	def play_game(self, didWin):
+		if didWin:
+			self.points += 3

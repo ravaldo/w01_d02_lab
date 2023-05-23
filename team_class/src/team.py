386981337adfc16dd911d61ba97d037f5ae0be8d
ravaldo <117010252+ravaldo@users.noreply.github.com>
2023-05-23 10:16:45 +0100

    fixed pic

--- team_class/src/team.py
+++ team_class/src/team.py
@@ -1,10 +1,11 @@
 
 class Team:
+	points = 0
 	def __init__(self, name, players, coach):
 		self.name = name
 		self.players = players
 		self.coach = coach
-		self.points = 0
+#		self.points = 0
 
 	def add_player(self, player):
 		self.players.append(player)
